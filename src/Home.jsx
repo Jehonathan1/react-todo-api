acc4bb67e0f24008219dd67f65ea8dde9c0008a2
Yehonatan Shechter <ydshechter@gmail.com>
2023-01-26 14:15:34 +0200

    reusing components to filter data

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,16 +5,18 @@ import TaskList from "./TaskList";
 const Home = () => {
 
     let [tasks, setTasks ] = useState([
-        { title: 'Make coffee', author: 'John', created_at: 'today', id:1},
-        { title: 'Change address', author: 'Bill', created_at: 'today', id:2},
-        { title: 'Build slideshow', author: 'Nancy', created_at: 'today', id:3}
+        { title: 'Make coffee', author: 'John', completed: true, id:1},
+        { title: 'Change address', author: 'Bill', completed: false, id:2},
+        { title: 'Build slideshow', author: 'Nancy', completed: true, id:3}
     ]);
 
     return ( 
        
        <div className="home">
           
-            <TaskList tasks={tasks} title={'All Tasks'} />
+            <TaskList tasks={tasks} title="All Tasks" />
+            <TaskList tasks={tasks.filter((task) => task.completed === true)} title=" Completed Tasks" />
+            <TaskList tasks={tasks.filter((task) => task.completed === false)} title=" Uncompleted Tasks" />
         
         </div>
      );
