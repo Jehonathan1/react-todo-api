7c197c789b94924d17116309709a52f1c3a42ff0
Yehonatan Shechter <ydshechter@gmail.com>
2023-01-26 12:22:48 +0200

    adding click events

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,22 @@
 const Home = () => {
-    return ( 
 
+    const handleClick = (e) => {
+           console.log('button clicked!', e.target)     
+    }
+    const handleClickAgain = (name, e) => {
+           console.log(`Hi there ${name}!`, e.target)     
+    }
+
+    return ( 
         <div className="home">
+            
             <h2>Homepage</h2>
+
+            {/* pass a function ref to the onClick event: */}
+            <button onClick={handleClick}>Click me!</button> 
+            <button onClick={(e) => handleClickAgain('John',e)}>Click me again!</button> 
+
         </div>
-        
      );
 }
  
