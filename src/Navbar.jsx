759759f42d35b57047568f2535578d2e87011fe6
Yehonatan Shechter <ydshechter@gmail.com>
2023-01-26 12:00:48 +0200

    initializing project with basic CSS

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Todo List</h1>
+            <div className="links">
+                <a href="#">Home</a>
+                <a href="#">New Todo</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
