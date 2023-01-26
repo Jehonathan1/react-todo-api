d6b177a9c5127b3a0061f2393099a16271eabe4b
Yehonatan Shechter <ydshechter@gmail.com>
2023-01-26 14:33:50 +0200

    passing function as prop

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,13 +10,20 @@ const Home = () => {
         { title: 'Build slideshow', author: 'Nancy', completed: true, id:3}
     ]);
 
+    // Delete tasks function
+    // create a new filtered task list and set it as the new list
+    // new list containd all tasks other than selected id 
+    const handleDelete = (id) => { 
+        const newTasks = tasks.filter(task => task.id !== id);
+        setTasks(newTasks);
+    }
     return ( 
        
        <div className="home">
           
-            <TaskList tasks={tasks} title="All Tasks" />
-            <TaskList tasks={tasks.filter((task) => task.completed === true)} title=" Completed Tasks" />
-            <TaskList tasks={tasks.filter((task) => task.completed === false)} title=" Uncompleted Tasks" />
+            <TaskList tasks={tasks} title="All Tasks" handleDelete={handleDelete} />
+            <TaskList tasks={tasks.filter((task) => task.completed === true)} title=" Completed Tasks" handleDelete={handleDelete} />
+            <TaskList tasks={tasks.filter((task) => task.completed === false)} title=" Uncompleted Tasks" handleDelete={handleDelete} />
         
         </div>
      );
--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,12 +1,16 @@
-const TaskList = ({tasks, title}) => { // destructure 'tasks' props 
+const TaskList = ({tasks, title, handleDelete}) => { // destructure 'tasks' props 
     return ( 
         <div className="task-list">
-
+           
+            {tasks.length !==0 && ( // show title onley if there are tasks
             <h2>{title}</h2>
+            )}
+
             {tasks.map((task) => (
                 <div className="task-preview" key={task.id}> 
-                <h2>{ task.title }</h2>
-                <p>{ task.author }</p>
+                    <h2>{ task.title }</h2>
+                    <p>{ task.author }</p>
+                    <button onClick={() => handleDelete(task.id)}>delete task</button>
                 </div>
             ))}
         </div>
