7e77ff89589c32a7bb6ba0e460e204c9a8caee83
Yehonatan Shechter <ydshechter@gmail.com>
2023-02-05 13:30:59 +0200

    added create task page

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState(''); // update the title state upon change
+    const [body, setBody] = useState(''); // update the body state upon change
     return ( 
         <div className="create">
             <h2>Add a new task</h2>
+            <form>
+            
+            <label>Task Title: </label>
+            <input 
+                type="text" 
+                required
+                value={title}
+                onChange = {(e) => setTitle(e.target.value)}
+            />
+            
+            <label>Task body: </label>
+            <textarea 
+                required
+                value={body}
+                onChange = {(e) => setBody(e.target.value)}
+            ></textarea>
+
+            <button>Add task</button>
+
+            <h4>{ title }</h4>
+            <p>{ body }</p>
+            </form>
         </div>
      );
 }
