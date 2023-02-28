328aadf7f3d2d25ab0c2cafde09acfc9c8c9266c
Yehonatan Shechter <ydshechter@gmail.com>
2023-02-28 08:20:35 +0200

    fixed home link in navbar

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
             <h1>Todo List</h1>
             <div className="links">
                 {/* Link to the home page */}
-                <Link to="/tasks">Home</Link>
+                <Link to="/">Home</Link>
                 {/* Link to create a new todo */}
                 <Link to="/create">New Todo</Link>
             </div>
